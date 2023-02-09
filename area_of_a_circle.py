04f5e8831c946c9ef33b4a67276f82239cf63584
jaysonocharo <mogakajayson@gmail.com>
2023-02-09 11:09:28 +0300

    Lesson 1

---
+++ area_of_a_circle.py
@@ -0,0 +1,8 @@
+#this program calculates
+#the area of a circle
+radius = 12
+pi = 3.14
+area = radius * radius * pi
+print(area)
+
+
---
+++ area_of_a_rectangle.py
@@ -0,0 +1,7 @@
+
+#this program calculates
+# area of a rectangle 
+length = 15
+width = 9
+area = length * width
+print(area)
